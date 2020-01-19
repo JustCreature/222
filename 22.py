484d56148f858affbf42174d05780bb34d4da939
Nikita Zavartsev <nikoss9644@gmail.com>
2020-01-19 16:46:31 +0600

    init

---
+++ 22.py
@@ -0,0 +1,22 @@
+import math
+
+arr = []
+
+def count(i):
+    rt = 'rrr'
+    while i < 10:
+        x = math.pi * math.sqrt(144) + i
+        arr.append(x)
+        i -= 2
+        i += 2
+        i += 2
+    print(arr)
+    print('Good')
+    print('Well')
+    for i in range(1, 10):
+        i = i += 3
+        str = "qqq"
+        print(i)
+    return arr
+
+print(count(3))
